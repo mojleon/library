d4cd9f983dff93d4817a015a66f63a12039c226d
mojleon <damirkalbic@gmail.com>
2022-04-23 20:17:04 +0200

    [CLASS] Added most of the code to a class, except the submit which somehow refreshes the page even though the function in the class says not to. For now, I just made a function that calls the class function... Not very happy about it.

--- script.js
+++ script.js
@@ -1,164 +1,128 @@
-let myLibrary = [
-  {
-    book: "Harry Potter",
-    author: "J. K. Rowling",
-    pages: 223,
-    read: false,
-    id: 1,
-  },
-  {
-    book: "Books of Blood",
-    author: "Clive Barker",
-    pages: 507,
-    read: false,
-    id: 2,
-  },
-  {
-    book: "Dune",
-    author: "Frank Herbert",
-    pages: 412,
-    read: true,
-    id: 3,
-  },
-];
+class bookClass {
+  myLibrary = [
+    {
+      book: "Harry Potter",
+      author: "J. K. Rowling",
+      pages: 223,
+      read: false,
+      id: 1,
+    },
+    {
+      book: "Books of Blood",
+      author: "Clive Barker",
+      pages: 507,
+      read: false,
+      id: 2,
+    },
+    {
+      book: "Dune",
+      author: "Frank Herbert",
+      pages: 412,
+      read: true,
+      id: 3,
+    },
+  ];
 
-function books() {
-  let flexBox = document.createElement("section");
-  flexBox.setAttribute("id", "books");
-  flexBox.setAttribute(
-    "style",
-    " display: flex; justify-content: center; align-items:flex-start; flex-wrap: wrap; gap: 10px; width: 1024px; height: auto;"
-  );
-  document.querySelector("body").appendChild(flexBox);
+  constructor() {
+    this.books();
+  }
 
-  myLibrary.forEach((b) => {
-    let div = document.createElement("div");
-    div.setAttribute(
+  books() {
+    let flexBox = document.createElement("section");
+    flexBox.setAttribute("id", "books");
+    flexBox.setAttribute(
       "style",
-      "padding: 50px; box-shadow: 3px 3px 15px #999; border-radius: 10px; position: relative; f"
+      " display: flex; justify-content: center; align-items:flex-start; flex-wrap: wrap; gap: 10px; width: 1024px; height: auto;"
     );
+    document.querySelector("body").appendChild(flexBox);
 
-    div.innerText =
-      "Book = " +
-      b.book +
-      "\n Author = " +
-      b.author +
-      "\n Pages = " +
-      b.pages +
-      "\n";
+    this.myLibrary.forEach((b) => {
+      let div = document.createElement("div");
+      div.setAttribute(
+        "style",
+        "padding: 50px; box-shadow: 3px 3px 15px #999; border-radius: 10px; position: relative; f"
+      );
 
-    div.innerHTML += `<input id='${b.id}_cb' type='checkbox' /><label for='${b.id}_cb'>Have you read the book?</label>`;
+      div.innerText =
+        "Book = " +
+        b.book +
+        "\n Author = " +
+        b.author +
+        "\n Pages = " +
+        b.pages +
+        "\n";
 
-    flexBox.appendChild(div);
+      div.innerHTML += `<input id='${b.id}_cb' type='checkbox' /><label for='${b.id}_cb'>Have you read the book?</label>`;
 
-    checkBoxButton(b.read, b.id);
+      flexBox.appendChild(div);
 
-    closeButton(b.id, div);
-  });
-}
+      this.checkBoxButton(b.read, b.id);
 
-function checkBoxButton(read, id) {
-  let checkbox = document.getElementById(`${id}_cb`);
-  checkbox.checked = read;
-
-  checkbox.addEventListener("click", function () {
-    checkbox.checked != checkbox.checked;
-    myLibraryCB = myLibrary.find((object) => object.id == id).read =
-      checkbox.checked;
-  });
-}
-
-function refreshView() {
-  document.querySelector("#books").remove();
-  books();
-}
-
-function closeButton(id, div) {
-  let close_img = document.createElement("img");
-  close_img.setAttribute(
-    "style",
-    "position: absolute; right: 10px; top: 10px; width: 30px"
-  );
-  close_img.addEventListener("click", function () {
-    const newLibrary = myLibrary.filter((a) => {
-      return a.id != id;
+      this.closeButton(b.id, div);
     });
-    myLibrary = newLibrary;
-    refreshView();
-  });
-  close_img.src = "./images/close.svg";
-  div.appendChild(close_img);
+  }
+
+  checkBoxButton(read, id) {
+    let checkbox = document.getElementById(`${id}_cb`);
+    checkbox.checked = read;
+
+    checkbox.addEventListener("click", function () {
+      checkbox.checked != checkbox.checked;
+      myLibraryCB = this.myLibrary.find((object) => object.id == id).read =
+        checkbox.checked;
+    });
+  }
+
+  refreshView() {
+    document.querySelector("#books").remove();
+    this.books();
+  }
+
+  closeButton(id, div) {
+    let close_img = document.createElement("img");
+
+    close_img.setAttribute(
+      "style",
+      "position: absolute; right: 10px; top: 10px; width: 30px"
+    );
+    close_img.addEventListener(
+      "click",
+      function () {
+        const newLibrary = this.myLibrary.filter((a) => {
+          return a.id != id;
+        });
+        this.myLibrary = newLibrary;
+        this.refreshView();
+      }.bind(this)
+    );
+    close_img.src = "./images/close.svg";
+    div.appendChild(close_img);
+  }
+
+  addBookToLibrary() {
+    document.querySelector("#addBookForm").classList.remove("hidden");
+  }
+
+  addBook() {
+    let e = window.event;
+    e.preventDefault();
+
+    let object = {
+      book: e.target[0].value,
+      author: e.target[1].value,
+      pages: e.target[2].value,
+      read: e.target[3].value,
+      id: this.myLibrary[this.myLibrary.length - 1].id + 1,
+    };
+    this.myLibrary.push(object);
+    document.querySelector("#addBookForm").classList.add("hidden");
+
+    this.refreshView();
+  }
 }
 
-function addBookToLibrary() {
-  document.querySelector("#addBookForm").classList.remove("hidden");
-}
-
-books();
+let book = new bookClass();
 
 function addBook() {
-  let e = window.event;
-  e.preventDefault();
-
-  let object = {
-    book: e.target[0].value,
-    author: e.target[1].value,
-    pages: e.target[2].value,
-    read: e.target[3].value,
-    id: myLibrary[myLibrary.length - 1].id + 1,
-  };
-  myLibrary.push(object);
-  document.querySelector("#addBookForm").classList.add("hidden");
-
-  refreshView();
+  book.addBook();
 }
-
-function addTwo(a) {
-  return (b) => {
-    return (c) => {
-      return a + b + c;
-    };
-  };
-}
-
-/* 
-* Just a few test I did to see whether this works.
-*
-console.log(addTwo(4)(3)(3));
-
-let million = 1_000_000;
-console.log(million);
-
-class someTestClass {
-  #shield() {
-    return "someTestClass";
-  }
-  noShield() {
-    return this.#shield();
-  }
-}
-
-let someTClass = new someTestClass();
-
-console.log(someTClass.noShield());
-
-// console.log(addBookToLibrary.prototype.test.name);
-
-let person = {
-  name: "John Doe",
-  getName: function () {
-    console.log(this.name);
-  },
-};
-
-setTimeout(person.getName.bind(person), 1000);
-
-(() => {
-  console.log("()()()");
-})();
-
-console.log(
-  !(function () {
-    return "";
-  })()
-); */
